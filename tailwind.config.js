1e0276aab56a2bebef1ab19db056084346a45861
csmasayon <70651638+csmasayon@users.noreply.github.com>
2023-06-01 06:57:12 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/[id].tsx",
+    "./pages/**/**/[id].{js,ts,jsx,tsx,mdx}",
  
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
